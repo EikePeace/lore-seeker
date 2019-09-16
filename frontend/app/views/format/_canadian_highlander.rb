8d5c64a5ed5bc6513b3adbcf759210a58ae86937
Fenhl <fenhl@fenhl.net>
2019-09-16 15:44:30 +0000

    Add format page for Canadian Highlander

---
+++ frontend/app/views/format/_canadian_highlander.rb
@@ -0,0 +1,12 @@
+%p
+  %em Canadian Highlander
+  is an unofficial competitive non-rotating Singleton Constructed format. It uses 100-card decks without commanders.
+
+%p
+  Canadian Highlander's most distinctive feature is the points list, which allows the restriction of powerful cards in a singleton format without resorting to power-level bans. A deck can have up to 10 points. Lore Seeker automatically updates to the newest points list at least once per week.
+
+%p
+  %a{href: "https://canadianhighlander.wordpress.com/"} Official website
+
+%p
+  = search_help "restricted:canlander", "All cards on the points list"
