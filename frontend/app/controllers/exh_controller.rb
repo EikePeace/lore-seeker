d36ae836bb574d288badfc600a3b6c4361df257e
Fenhl <fenhl@fenhl.net>
2019-10-05 06:42:55 +0000

    Move ExhSorter class into exh_controller.rb

--- frontend/app/controllers/exh_controller.rb
+++ frontend/app/controllers/exh_controller.rb
@@ -1,3 +1,27 @@
+class ExhSorter
+  def initialize(ech)
+    @ech = ech
+  end
+
+  def sort(results)
+    results.sort_by do |c|
+      [
+        -c.num_exh_votes,
+        @ech.legality(c).start_with?("banned"),
+        @ech.legality(c).start_with?("restricted"),
+        !c.commander?,
+        c.release_date_i,
+        .color_identity.size,
+        c.default_sort_index
+      ]
+    end
+  end
+
+  def warnings
+    []
+  end
+end
+
 class ExhController < ApplicationController
   def index
     redirect_to(controller: "format", action: "show", id: "elder-xmage-highlander")
