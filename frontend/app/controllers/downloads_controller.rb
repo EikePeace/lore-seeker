51e7b61534b6d12739bd43bee126329f4e1ba52e
Fenhl <fenhl@fenhl.net>
2020-02-08 23:39:02 +0000

    Show current BCB version on downloads page

--- frontend/app/controllers/downloads_controller.rb
+++ frontend/app/controllers/downloads_controller.rb
@@ -1,5 +1,7 @@
 class DownloadsController < ApplicationController
   def index
     @title = "Downloads"
+    version_file = (Pathname(__dir__) + "../../../data/bcb-version.txt")
+    @version = version_file.exist? ? version_file.read : "unknown"
   end
 end
