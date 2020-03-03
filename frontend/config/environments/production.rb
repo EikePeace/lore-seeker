fc39084c1f7deaa14393825b84b22f5402aa37ea
Fenhl <fenhl@fenhl.net>
2020-03-03 22:27:23 +0000

    Fix production environment to reenable dev

--- frontend/config/environments/production.rb
+++ frontend/config/environments/production.rb
@@ -1,7 +1,7 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
-  # allow debugging on production
+  config.hosts << "dev.lore-seeker.cards"
   config.hosts << "lore-seeker.cards"
 
   # Code is not reloaded between requests.
